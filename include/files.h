52382ab853a8110845def7e41191d932f134dff5
yemiekai <418504286@qq.com>
2021-01-04 23:42:38 +0800

    add 1

---
+++ include/files.h
@@ -0,0 +1,13 @@
+#ifndef _FILES_H_
+#define _FILES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream> 
+
+#include <vector>
+
+class Solution1;
+
+
+#endif
---
+++ src/1_TwoSum.cpp
@@ -0,0 +1,9 @@
+#include "include/files.h"
+
+
+class Solution1 {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+
+    }
+};
