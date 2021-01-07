ced4815456836db87a83c0c304901cc4efee0a48
yemiekai <418504286@qq.com>
2021-01-07 09:31:18 +0800

    add question 1

--- src/1_TwoSum.cpp
+++ src/1_TwoSum.cpp
@@ -5,11 +5,20 @@
 std::vector<int> Solution1::twoSum(std::vector<int>& nums, int target){
     std::map<int, int> mmap;
     std::vector<int> result;
-    int tmp;
 
-    for(int n : nums){
-        std::cout << n << std::endl;
+    int size = nums.size();
+    int matchNum;
 
+    // 遍历数组
+    for(int i = 0; i < size; i++){
+        matchNum = target - nums[i];  // 你要找到数
+
+        // 看看有没有
+        if(mmap.find(matchNum) != mmap.end()){
+            return std::vector<int>{mmap[matchNum], i};  // 返回索引
+        }
+
+        mmap[nums[i]] = i;  // 索引存起来, 继续下一轮寻找
     }
 
     return result;
