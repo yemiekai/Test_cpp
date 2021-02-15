08b963d9052586b8d5a03e4a6b0a7c0ed746f9eb
yemiekai <418504286@qq.com>
2021-02-15 11:39:57 +0800

    add 9

---
+++ include/9_PalindromeNumber.h
@@ -0,0 +1,21 @@
+#ifndef _9_PALINDROME_H_
+#define _9_PALINDROME_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream> 
+#include <string>
+
+#include <vector>
+#include <map>
+#include <set>
+
+class Solution9 {
+public:
+    void test();
+    bool isPalindrome(int x);
+
+
+};
+
+#endif
