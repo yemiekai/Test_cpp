3a6608d7f73b045e696717cb014885a36853f96a
yemiekai <418504286@qq.com>
2021-03-24 14:27:52 +0800

    adjust files

---
+++ leetcode/include/31_NextPermutation.h
@@ -0,0 +1,22 @@
+#ifndef _31_NEXT_PERMUTATION_H
+#define _31_NEXT_PERMUTATION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream> 
+#include <string>
+
+#include <vector>
+#include <map>
+#include <set>
+
+// 31 下一个排列
+class Solution31 {
+public:
+    void nextPermutation(std::vector<int>& nums);
+   
+    void test();
+
+};
+
+#endif
